afaaddf700e16b63e07ebaeb2a75effeb74fbf64
Tulio-Rangel <tjrn20@hotmail.com>
2023-07-14 15:41:07 -0500

    update main class with income screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'presentation/expense_form.dart';
 import 'presentation/expense_list.dart';
+import 'presentation/income_form.dart';
+import 'presentation/income_list.dart';
 import 'application/add_expense_use_case.dart';
+import 'application/add_income_use_case.dart';
 import 'application/expense_repository.dart';
+import 'application/income_repository.dart';
 
 void main() {
   runApp(ControlDeGastosApp());
@@ -12,6 +16,10 @@ class ControlDeGastosApp extends StatelessWidget {
   final ExpenseRepository _expenseRepository = ExpenseRepositoryImpl();
   final AddExpenseUseCase _addExpenseUseCase =
       AddExpenseUseCase(ExpenseRepositoryImpl());
+
+  final IncomeRepository _incomeRepository = IncomeRepositoryImpl();
+  final AddIncomeUseCase _addIncomeUseCase =
+      AddIncomeUseCase(IncomeRepositoryImpl());
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +31,7 @@ class ControlDeGastosApp extends StatelessWidget {
       home: ExpenseList(_expenseRepository),
       routes: {
         '/expenseForm': (ctx) => ExpenseForm(_addExpenseUseCase),
+        '/incomeForm': (ctx) => IncomeForm(_addIncomeUseCase),
       },
     );
   }
