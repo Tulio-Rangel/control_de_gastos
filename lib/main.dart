7c3cd1431521996ce471923f91b6e717580f5d5d
Tulio-Rangel <tjrn20@hotmail.com>
2023-07-14 15:54:41 -0500

    delete de home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
-import 'presentation/home_screen.dart';
+import 'presentation/expense_form.dart';
+import 'presentation/expense_list.dart';
+import 'presentation/income_form.dart';
+import 'presentation/income_list.dart';
+import 'application/add_expense_use_case.dart';
+import 'application/add_income_use_case.dart';
+import 'application/expense_repository.dart';
+import 'application/income_repository.dart';
 
 void main() {
   runApp(ControlDeGastosApp());
 }
 
 class ControlDeGastosApp extends StatelessWidget {
+  final ExpenseRepository _expenseRepository = ExpenseRepositoryImpl();
+  final AddExpenseUseCase _addExpenseUseCase =
+      AddExpenseUseCase(ExpenseRepositoryImpl());
+
+  final IncomeRepository _incomeRepository = IncomeRepositoryImpl();
+  final AddIncomeUseCase _addIncomeUseCase =
+      AddIncomeUseCase(IncomeRepositoryImpl());
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +28,11 @@ class ControlDeGastosApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: ExpenseList(_expenseRepository),
+      routes: {
+        '/expenseForm': (ctx) => ExpenseForm(_addExpenseUseCase),
+        '/incomeForm': (ctx) => IncomeForm(_addIncomeUseCase),
+      },
     );
   }
 }
