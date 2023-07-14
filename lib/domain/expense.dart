280b4385dc97670f0247b188402e02cd494dfe0a
Tulio-Rangel <tjrn20@hotmail.com>
2023-07-14 13:55:33 -0500

    create domain classes

---
+++ lib/domain/expense.dart
@@ -0,0 +1,11 @@
+class Expense {
+  final double amount;
+  final DateTime date;
+  final String category;
+
+  Expense({
+    required this.amount,
+    required this.date,
+    required this.category,
+  });
+}
---
+++ lib/domain/income.dart
@@ -0,0 +1,11 @@
+class Income {
+  final double amount;
+  final DateTime date;
+  final String category;
+
+  Income({
+    required this.amount,
+    required this.date,
+    required this.category,
+  });
+}
