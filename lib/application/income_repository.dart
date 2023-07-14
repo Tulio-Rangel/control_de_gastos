afe3adcfe6cb925918b0f7714fb6a2a09bbaba85
Tulio-Rangel <tjrn20@hotmail.com>
2023-07-14 15:39:01 -0500

    update incomeRepository with IncomeRepositoryImpl

--- lib/application/income_repository.dart
+++ lib/application/income_repository.dart
@@ -5,3 +5,17 @@ abstract class IncomeRepository {
   List<Income> getAllIncomes();
   // Otros métodos relacionados con la manipulación de ingresos
 }
+
+class IncomeRepositoryImpl implements IncomeRepository {
+  List<Income> _incomes = [];
+
+  @override
+  void saveIncome(Income income) {
+    _incomes.add(income);
+  }
+
+  @override
+  List<Income> getAllIncomes() {
+    return _incomes;
+  }
+}
