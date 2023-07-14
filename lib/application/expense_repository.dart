c0c826ec3fde1d118b0989e12207814e7fedea3b
Tulio-Rangel <tjrn20@hotmail.com>
2023-07-14 15:02:52 -0500

    add more function to Expense Repository class

--- lib/application/expense_repository.dart
+++ lib/application/expense_repository.dart
@@ -5,3 +5,17 @@ abstract class ExpenseRepository {
   List<Expense> getAllExpenses();
   // Otros métodos relacionados con la manipulación de gastos
 }
+
+class ExpenseRepositoryImpl implements ExpenseRepository {
+  List<Expense> _expenses = [];
+
+  @override
+  void saveExpense(Expense expense) {
+    _expenses.add(expense);
+  }
+
+  @override
+  List<Expense> getAllExpenses() {
+    return _expenses;
+  }
+}
