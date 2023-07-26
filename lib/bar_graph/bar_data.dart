f8f05e0ecf463ec9a520365d6b694d16577c89b4
Tulio-Rangel <tjrn20@hotmail.com>
2023-07-26 15:08:03 -0500

    create a model for bar (graph)

---
+++ lib/bar_graph/bar_data.dart
@@ -0,0 +1,42 @@
+import 'package:control_de_gastos/bar_graph/individual_bar.dart';
+
+class BarData {
+  final double monAmount;
+  final double tueAmount;
+  final double wedAmount;
+  final double thurAmount;
+  final double friAmount;
+  final double satAmount;
+  final double sunAmount;
+
+  BarData(this.monAmount, this.tueAmount, this.wedAmount, this.thurAmount,
+      this.friAmount, this.satAmount, this.sunAmount);
+
+  List<IndividualBar> barData = [];
+
+  // Initialize bar data
+  void initializeBarData() {
+    barData = [
+      // Monday
+      IndividualBar(x: 0, y: monAmount),
+
+      // Tuesday
+      IndividualBar(x: 1, y: tueAmount),
+
+      // Wednesday
+      IndividualBar(x: 2, y: wedAmount),
+
+      // Thursday
+      IndividualBar(x: 3, y: thurAmount),
+
+      // Friday
+      IndividualBar(x: 4, y: friAmount),
+
+      // Saturday
+      IndividualBar(x: 5, y: satAmount),
+
+      // Sunday
+      IndividualBar(x: 6, y: sunAmount),
+    ];
+  }
+}
---
+++ lib/bar_graph/individual_bar.dart
@@ -0,0 +1,6 @@
+class IndividualBar {
+  final int x; // Position on x axis
+  final double y; // Amount is $
+
+  IndividualBar({required this.x, required this.y});
+}
