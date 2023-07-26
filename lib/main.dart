23fb52a98bd34a93f337383280fba78b8d85df4e
Tulio-Rangel <tjrn20@hotmail.com>
2023-07-25 23:18:35 -0500

    tercer intento

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,11 @@
 import 'package:flutter/material.dart';
-import 'presentation/expense_form.dart';
-import 'presentation/expense_list.dart';
-import 'presentation/income_form.dart';
-import 'presentation/income_list.dart';
-import 'application/add_expense_use_case.dart';
-import 'application/add_income_use_case.dart';
-import 'application/expense_repository.dart';
-import 'application/income_repository.dart';
 
 void main() {
   runApp(ControlDeGastosApp());
 }
 
 class ControlDeGastosApp extends StatelessWidget {
-  final ExpenseRepository _expenseRepository = ExpenseRepositoryImpl();
-  final AddExpenseUseCase _addExpenseUseCase =
-      AddExpenseUseCase(ExpenseRepositoryImpl());
-
-  final IncomeRepository _incomeRepository = IncomeRepositoryImpl();
-  final AddIncomeUseCase _addIncomeUseCase =
-      AddIncomeUseCase(IncomeRepositoryImpl());
+  const ControlDeGastosApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,11 +14,7 @@ class ControlDeGastosApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ExpenseList(_expenseRepository),
-      routes: {
-        '/expenseForm': (ctx) => ExpenseForm(_addExpenseUseCase),
-        '/incomeForm': (ctx) => IncomeForm(_addIncomeUseCase),
-      },
+      home: ControlDeGastosApp(),
     );
   }
 }
