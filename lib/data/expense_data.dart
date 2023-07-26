c0aba405e8c792fdc8280fba0d977e090d912292
Tulio-Rangel <tjrn20@hotmail.com>
2023-07-25 23:51:28 -0500

    create the expense data

---
+++ lib/data/expense_data.dart
@@ -0,0 +1,87 @@
+import 'package:control_de_gastos/models/expense_item.dart';
+
+class ExpenseData {
+  // List of All expenses
+  List<ExpenseItem> overallExpenseList = [];
+
+  // Gewet Expense list
+  List<ExpenseItem> getAllExpenseList() {
+    return overallExpenseList;
+  }
+
+  // Add new expense
+  void addNewExpense(ExpenseItem newExpense) {
+    overallExpenseList.add(newExpense);
+  }
+
+  // Delete expense
+  void deleteExpense(ExpenseItem expense) {
+    overallExpenseList.remove(expense);
+  }
+
+  // Get weekday (mon, tues, etc) from a dateTime object
+  String getDayName(DateTime dateTime) {
+    switch (dateTime.weekday) {
+      case 1:
+        return 'Mon';
+      case 2:
+        return 'Tue';
+      case 3:
+        return 'Wed';
+      case 4:
+        return 'Thur';
+      case 5:
+        return 'Fri';
+      case 6:
+        return 'Sat';
+      case 7:
+        return 'Sun';
+      default:
+        return '';
+    }
+  }
+
+  // Get the date for start of the week (monday)
+  DateTime startOfWeekDate() {
+    DateTime? startOfWeek;
+
+    // Get today's date
+    DateTime today = DateTime.now();
+
+    // Go backwards from today to find monday
+    for (int i = 0; i < 7; i++) {
+      if (getDayName(today.subtract(Duration(days: i))) == 'Mon') {
+        startOfWeek = today.subtract(Duration(days: i));
+      }
+    }
+    return startOfWeek!;
+  }
+
+  /**
+   * Convert overall lista of expenses into a daily exoense sumary
+   * 
+   * e.g
+   * 
+   * overallExpenseList = [
+   * 
+   * [food, 2023/01/30, $10],
+   * [game, 2023/01/30, $100],
+   * [bill, 2023/02/14, $30],
+   * [phone, 2023/02/21, $600],
+   * [phone, 2023/02/21, $200],
+   * [phone, 2023/02/21, $300],
+   * 
+   * ]
+   * 
+   * ->
+   * 
+   * DailyExpenseSumary = [
+   * 
+   * [2023/01/10: $110],
+   * [2023/02/14: $30],
+   * [2023/02/21: $1100]
+   * 
+   * ]
+   * 
+   */
+}
