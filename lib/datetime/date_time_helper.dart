2ce4aa38c2318980c3094d7f6306029fb82040d9
Tulio-Rangel <tjrn20@hotmail.com>
2023-07-26 00:02:03 -0500

    add format to month and day in date helper

--- lib/datetime/date_time_helper.dart
+++ lib/datetime/date_time_helper.dart
@@ -5,9 +5,15 @@ String convertDateTimeToString(DateTime dateTime) {
 
   // Month in format -> mm
   String month = dateTime.month.toString();
+  if (month.length == 1) {
+    month = '0$month';
+  }
 
   // Day format -> dd
   String day = dateTime.day.toString();
+  if (day.length == 1) {
+    day = '0$day';
+  }
 
   // Final format
   String yyyymmdd = year + month + day;
