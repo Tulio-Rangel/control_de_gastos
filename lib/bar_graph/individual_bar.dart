41440e38630f6d10a85db86230a07bc982774f76
Tulio-Rangel <tjrn20@hotmail.com>
2023-11-01 15:08:36 -0500

    delete everything

--- lib/bar_graph/individual_bar.dart
+++
@@ -1,6 +0,0 @@
-class IndividualBar {
-  final int x; // Position on x axis
-  final double y; // Amount is $
-
-  IndividualBar({required this.x, required this.y});
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
-import 'package:control_de_gastos/data/expense_data.dart';
 import 'package:control_de_gastos/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
-import 'package:provider/provider.dart';
 
 void main() async {
-  // Initialize hive
-  await Hive.initFlutter();
-
-  // Open a hive box
-  await Hive.openBox('expense_database');
-
   runApp(const ControlDeGastosApp());
 }
 
@@ -19,14 +10,10 @@ class ControlDeGastosApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ExpenseData(),
-      builder: ((context, child) => MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-                useMaterial3: true, colorSchemeSeed: Colors.greenAccent),
-            home: const HomePage(),
-          )),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.greenAccent),
+      home: const HomePage(),
     );
   }
 }
